b1dd4b43c57c94ca5ce70c044804e248271466ab
Adeolu David <adeolusegun99@gmail.com>
2023-02-25 02:04:23 +0100

    done with homepage

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, View } from "react-native"
+import products from "./src/data/products"
+import ProductScreen from "./src/screens/ProductScreen"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ProductScreen />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+})
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,34 @@
+import { Image, StyleSheet, Text, View, FlatList } from "react-native"
+import React from "react"
+import products from "../data/products"
+const ProductScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: item.image,
+            }}
+            style={styles.image}
+          />
+        </View>
+      )}
+      numColumns={2}
+    />
+  )
+}
+
+export default ProductScreen
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+})
